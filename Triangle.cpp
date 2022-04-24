2e003391943a4d1ba46d511f624c8a92e8f55497
AlexeyRybalkin <rybalkin@sfedu.ru>
2022-04-24 16:00:45 +0300

    Triangle

--- Triangle.cpp
+++ Triangle.cpp
@@ -3,3 +3,45 @@
 //
 
 #include "Triangle.h"
+#include <iostream>
+
+Triangle::Triangle(int a, int b, int c) : Figure(convertLengthsToPoints(a, b, c), 3)
+{
+
+}
+
+Triangle::~Triangle()
+{
+	std::cout << "I am destructor \n";
+}
+
+Point** Triangle::convertLengthsToPoints(int a, int b, int c)
+{
+	Point** point = new Point *[3];
+	point[0] = new Point(a, 0);
+	point[1] = new Point(a, b);
+	point[2] = new Point(b, c);
+
+	return point;
+}
+
+
+int Triangle::perimeter()
+{
+	std::cout << "Figure Implementation perimeter" << std::endl;
+	double val = 0;
+	if (points == nullptr || numberOfPoints == 0) {
+		return 0;
+	}
+	for (int i = 0; i < this->numberOfPoints - 1; i++) {
+		val += this->points[i]->distance(this->points[i + 1]);
+	}
+	val += this->points[this->numberOfPoints - 1]->distance(this->points[0]);
+	return (int) val;
+}
+
+int Triangle::area()
+{
+	std::cout << "Rectangle Implementation area" << std::endl;
+	return (int)(this->points[0]->distance(this->points[1]) * this->points[1]->distance(this->points[2]));
+}
--- Triangle.h
+++ Triangle.h
@@ -5,14 +5,28 @@
 #ifndef CLASSES_TRIANGLE_H
 #define CLASSES_TRIANGLE_H
 
+#include "Figure.h"
+
 /**
  * TODO extend this Class from Figure class and Implement constructor, destructor,  perimeter and area methods in Triangle.cpp file
  */
-class Triangle {
+class Triangle : public Figure {
 //TODO put your code here
 //you should define methods and constructor/destructor prototypes here and implement them in Triangle.cpp file
 //constructor must use the pointers to 3 apexes of the triangle.
+private:
 
+	int a, b, c;
+
+public:
+
+	Triangle(int a, int b, int c);
+	~Triangle();
+
+	Point** convertLengthsToPoints(int a, int b, int c);
+
+	int perimeter() override;
+	int area() override;
 };
 
 
